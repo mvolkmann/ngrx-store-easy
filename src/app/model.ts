c33ebdfd8253388ce03b80e77c3ec17868fba904
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2018-04-06 12:38:25 -0500

    removed some commented code

--- src/app/model.ts
+++ src/app/model.ts
@@ -1,5 +1,3 @@
-import {createSelector} from '@ngrx/store';
-
 export interface Person {
   age: number;
   colors: string[];
@@ -15,11 +13,3 @@ export interface AppState {
   newColor: string;
   person: Person;
 }
-
-/*
-const makeSelector = fn => createSelector(x => x, fn);
-
-export const personDirectionSelector = makeSelector(
-  state => state.person.direction
-);
-*/
